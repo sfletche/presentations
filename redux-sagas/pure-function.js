a1f46a46051196c7ca3a7e99d59fdf6869b259c8
sfletche <sfletche@gmail.com>
2018-01-18 17:09:20 -0800

    updating redux-saga presentation

--- redux-sagas/pure-function.js
+++ redux-sagas/pure-function.js
@@ -1,48 +1,50 @@
 
 
 // non-pure function (non-deterministic and mutable)
-function increment(obj = {}) {
+function increment(obj) {
 	obj.count = obj.count ? object.count + incrementVal : incrementVal;
 	return obj;
 }
 
 > var incrementVal = 1;
 > const myObject = { count: 41 };
-> const incrementedObject = increment(myObject);
+> let incrementedObject = increment(myObject);
 > console.log(incrementedObject.count); // 42
 > console.log(myObject.count); // 42
 
-> var incrementVal = 2;
-> const myObject = { count: 41 };
-> const incrementedObject = increment(myObject);
-> console.log(incrementedObject.count); // 43
-> console.log(myObject.count); // 43
+> incrementVal = 10;
+> incrementedObject = increment(myObject);
+> console.log(incrementedObject.count); // 52
+> console.log(myObject.count); // 52
 
 
-// still a non-pure function (deterministic but mutable)
-function increment(obj = {}) {
-	const incrementVal = 1;
+// still a non-pure function (deterministic but still mutable)
+function increment(obj, incrementVal) {
 	obj.count = obj.count ? object.count + incrementVal : incrementVal;
 	return obj;
 }
 
+> const incrementVal = 1;
 > const myObject = { count: 41 };
-> const incrementedObject = increment(myObject);
+> let incrementedObject = increment(myObject, incrementVal);
 > console.log(incrementedObject.count); // 42
 > console.log(myObject.count); // 42
 
 
-// pure function (deterministic and mutable)
-function increment(obj = {}) {
-	const incrementVal = 1;
+// pure function (deterministic and immutable)
+function increment(obj, incrementVal) {
 	return {
 		...obj,
 		count: obj.count ? object.count + incrementVal : incrementVal,
 	}
 }
 
+> const incrementVal = 1;
 > const myObject = { count: 41 };
-> const incrementedObject = increment(myObject);
+> let incrementedObject = increment(myObject, incrementVal);
 > console.log(incrementedObject.count); // 42
 > console.log(myObject.count); // 41
 
+> incrementedObject = increment(myObject, incrementVal);
+> console.log(incrementedObject.count); // 42
+> console.log(myObject.count); // 41
